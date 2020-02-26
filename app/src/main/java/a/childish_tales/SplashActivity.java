e749bca40595bce4a88e8788b6245408b0f46283
amingoli78 <amin.goli78@gmail.com>
2020-02-26 22:31:16 +0330

    crate activity splash

---
+++ app/src/main/java/a/childish_tales/SplashActivity.java
@@ -0,0 +1,14 @@
+package a.childish_tales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

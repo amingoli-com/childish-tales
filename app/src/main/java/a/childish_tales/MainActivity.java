62c41050b330b78dd83a1c394bbedc6376f21e6c
amingoli78 <amin.goli78@gmail.com>
2020-02-27 00:17:26 +0330

    fix Slide

--- app/src/main/java/a/childish_tales/MainActivity.java
+++ app/src/main/java/a/childish_tales/MainActivity.java
@@ -1,17 +1,10 @@
 package a.childish_tales;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.dingmouren.layoutmanagergroup.slide.ItemConfig;
-import com.dingmouren.layoutmanagergroup.slide.ItemTouchHelperCallback;
-import com.dingmouren.layoutmanagergroup.slide.OnSlideListener;
-import com.dingmouren.layoutmanagergroup.slide.SlideLayoutManager;
-
+import com.dingmouren.layoutmanagergroup.skidright.SkidRightLayoutManager;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +15,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     AdapterTest mAdapter;
 
-    ItemTouchHelperCallback mItemTouchHelperCallback;
-    ItemTouchHelper mItemTouchHelper;
-    SlideLayoutManager mSlideLayoutManager;
+    SkidRightLayoutManager mSkidRightLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,41 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
         add();
 
-        mItemTouchHelperCallback = new ItemTouchHelperCallback(mRecyclerView.getAdapter(), itemIntroList);
-        mItemTouchHelper = new ItemTouchHelper(mItemTouchHelperCallback);
-        mSlideLayoutManager = new SlideLayoutManager(mRecyclerView, mItemTouchHelper);
-        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
-        mRecyclerView.setLayoutManager(mSlideLayoutManager);
-
-        mItemTouchHelperCallback.setOnSlideListener(new OnSlideListener() {
-
-            @Override
-            public void onSliding(RecyclerView.ViewHolder viewHolder, float ratio, int direction) {
-                if (direction == ItemConfig.SLIDING_LEFT) {
-                    Log.e(TAG, "onSliding-SLIDED_LEFT");
-                } else if (direction == ItemConfig.SLIDING_RIGHT) {
-                    Log.e(TAG, "onSliding-SLIDING_RIGHT");
-                }
-            }
-
-            @Override
-            public void onSlided(RecyclerView.ViewHolder viewHolder, Object o, int direction) {
-                if (direction == ItemConfig.SLIDED_LEFT) {
-                    Log.e(TAG, "SLIDED_LEFT");
-                } else if (direction == ItemConfig.SLIDED_RIGHT) {
-                    Log.e(TAG, "SLIDED_RIGHT");
-                }
-                int position = viewHolder.getAdapterPosition();
-                Log.e(TAG, "onSlided--position:" + position);
-            }
-
-            @Override
-            public void onClear() {
-                add();
-                mAdapter.notifyDataSetChanged();
-                mRecyclerView.getAdapter();
-            }
-        });
+        mSkidRightLayoutManager = new SkidRightLayoutManager(1.80f, 0.5f);
+        mRecyclerView.setLayoutManager(mSkidRightLayoutManager);
     }
 
     void add(){

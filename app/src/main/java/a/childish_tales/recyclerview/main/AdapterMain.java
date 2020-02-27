0cf53dcc4080d7ad44d41b6253e326c72af153d7
amingoli78 <amin.goli78@gmail.com>
2020-02-27 16:16:32 +0330

    other fix

--- app/src/main/java/a/childish_tales/recyclerview/main/AdapterMain.java
+++ app/src/main/java/a/childish_tales/recyclerview/main/AdapterMain.java
@@ -1,10 +1,12 @@
 package a.childish_tales.recyclerview.main;
 
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 

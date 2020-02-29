a8d2c063dc8cf69c090c2cfadad6279fda391161
amingoli78 <amin.goli78@gmail.com>
2020-02-29 17:03:05 +0330

    remove

--- app/src/main/java/a/childish_tales/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package a.childish_tales.util;
-
-public class Constants {
-  public static final String PREF_APP_LOCATION = "appLocation";
-  public static final String PREF_VERSION = "version";
-}

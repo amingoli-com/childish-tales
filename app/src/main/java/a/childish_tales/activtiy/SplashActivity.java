792958ade7eb342bf00829537ac375a0d38a637f
amingoli78 <amin.goli78@gmail.com>
2020-03-10 16:33:22 +0330

    change file

--- app/src/main/java/a/childish_tales/activtiy/SplashActivity.java
+++ app/src/main/java/a/childish_tales/activtiy/SplashActivity.java
@@ -11,6 +11,16 @@ import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 
+import com.downloader.Error;
+import com.downloader.OnCancelListener;
+import com.downloader.OnDownloadListener;
+import com.downloader.OnPauseListener;
+import com.downloader.OnProgressListener;
+import com.downloader.OnStartOrResumeListener;
+import com.downloader.PRDownloader;
+import com.downloader.PRDownloaderConfig;
+import com.downloader.Progress;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -18,6 +28,7 @@ import java.io.IOException;
 import java.util.Random;
 
 import a.childish_tales.R;
+import a.childish_tales.util.file.FileUtil;
 import a.childish_tales.util.file.UnzipFirstFile;
 
 public class SplashActivity extends AppCompatActivity {
@@ -27,10 +38,58 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
-        File directory = contextWrapper.getDir(getFilesDir().getName(), Context.MODE_PRIVATE);
-        String basePath = directory.getAbsolutePath();
-        new UnzipFirstFile(this);
+
+        PRDownloader.initialize(getApplicationContext());
+
+// Enabling database for resume support even after the application is killed:
+        PRDownloaderConfig config = PRDownloaderConfig.newBuilder()
+                .setDatabaseEnabled(true)
+                .build();
+        PRDownloader.initialize(getApplicationContext(), config);
+
+// Setting timeout globally for the download network requests:
+        PRDownloaderConfig config2 = PRDownloaderConfig.newBuilder()
+                .setReadTimeout(30_000)
+                .setConnectTimeout(30_000)
+                .build();
+        PRDownloader.initialize(getApplicationContext(), config2);
+
+
+
+
+        int downloadId =
+                PRDownloader.download(
+                        "https://khadije.com/static/images/homepage-v3/salavat.jpg",
+                        FileUtil.getBaseDirectory(this),
+                        "test.jpg")
+                .build()
+                .setOnStartOrResumeListener(() -> {
+                    Log.d(TAG, "setOnStartOrResumeListener: ");
+                })
+                .setOnPauseListener(() -> {
+                    Log.d(TAG, "setOnPauseListener: ");
+                })
+                .setOnCancelListener(() -> {
+                    Log.d(TAG, "setOnCancelListener: ");
+                })
+                .setOnProgressListener(progress -> {
+                    Log.d(TAG, "setOnProgressListener: "+progress);
+                })
+                .start(new OnDownloadListener() {
+                    @Override
+                    public void onDownloadComplete() {
+                        Log.d(TAG, "onDownloadComplete: ");
+                    }
+
+                    @Override
+                    public void onError(Error error) {
+                        Log.d(TAG, "onError: "+error);
+                    }
+                });
+
+
+
+//        new UnzipFirstFile(this);
 
 //        String root = Environment.getExternalStorageDirectory().toString();
         String root = getExternalFilesDir(null).getAbsolutePath();

57af260f0afac0c2d89851ae09e00fe16395785b
amingoli78 <amin.goli78@gmail.com>
2020-03-23 15:15:19 +0430

    add Application Manager

---
+++ app/src/main/java/a/childish_tales/manager/ApplicationManager.java
@@ -0,0 +1,50 @@
+package a.childish_tales.manager;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class ApplicationManager extends Application {
+
+    public static final String TAG = "amingoli78-application";
+    private RequestQueue mRequestQueue;
+    private static ApplicationManager mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized ApplicationManager getInstance() {
+        return mInstance;
+    }
+
+    public  RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}

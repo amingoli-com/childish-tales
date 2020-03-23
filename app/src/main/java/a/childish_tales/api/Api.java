ee5116055cfd4ab083ddd120989c5dac1b789623
amingoli78 <amin.goli78@gmail.com>
2020-03-23 15:25:23 +0430

    fix Api

--- app/src/main/java/a/childish_tales/api/Api.java
+++ app/src/main/java/a/childish_tales/api/Api.java
@@ -3,37 +3,16 @@ package a.childish_tales.api;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.toolbox.StringRequest;
-import org.json.JSONException;
-import org.json.JSONObject;
 import a.childish_tales.manager.ApplicationManager;
 
 public class Api {
     public static void GET(String url, ApiListener listener){
-        StringRequest sendDelRQ = new StringRequest(Request.Method.GET, url, response ->
-        {
-            try {
-                JSONObject mainObject = new JSONObject(response);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }, Throwable::printStackTrace);
+        StringRequest sendDelRQ = new StringRequest
+                (Request.Method.GET, url, listener::onResult, e -> listener.onFiled());
         sendDelRQ.setRetryPolicy(new DefaultRetryPolicy(
-                5 * 1000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         ApplicationManager.getInstance().addToRequestQueue(sendDelRQ);
-    }
-
-
-    public static String GET_MAIN_URL(){
-        GET("", new ApiListener() {
-            @Override
-            public String onResult(String result) {
-                return result;
-            }
-
-            @Override
-            public void onFiled() {
-            }
-        });
-        return null;
     }
 }

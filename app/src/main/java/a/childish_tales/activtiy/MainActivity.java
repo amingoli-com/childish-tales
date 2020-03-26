33ad08bad5a070dce92366751991066b36dcb784
amingoli78 <amin.goli78@gmail.com>
2020-03-27 02:41:39 +0430

    clean

--- app/src/main/java/a/childish_tales/activtiy/MainActivity.java
+++ app/src/main/java/a/childish_tales/activtiy/MainActivity.java
@@ -3,18 +3,13 @@ package a.childish_tales.activtiy;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Iterator;
-
 import a.childish_tales.R;
 import a.childish_tales.manager.SaveManager;
 import a.childish_tales.recyclerview.multi.MultiAdaptor;
@@ -44,153 +39,9 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.recycler_view);
         mAdapter = new MultiAdaptor(itemIntroList,this);
         mRecyclerView.setAdapter(mAdapter);
-
-//        addItemFromJson();
-        String image = "https://github.com/amingoli78/childish-tales/raw/master/image/creation.png";
-        String title = "حضرت مهدی (عج)";
-
         JSON_SETER();
-
-        /*itemIntroList.add(IMAGE(image,title));
-        itemIntroList.add(IMAGE(image));
-        itemIntroList.add(TITLE(title));
-        additem_lakcheri();
-        itemIntroList.add(TITLE(title));
-        additem_h();
-        itemIntroList.add(TITLE(title));
-        additem_v();*/
-
         layoutManager= new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
         mRecyclerView.setLayoutManager(layoutManager);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
-    MultiItem SET_ITEM_SLIDER_LAKCHERI(ArrayList<MultiItem> arrayList){
-        MultiItem item = new MultiItem();
-        item.setType(MultiItem.SLIDER_LAKCHERI);
-        item.setArrayList(arrayList);
-        return item;
-    }
-
-    MultiItem SLIDER_HORIZONTAL(ArrayList<MultiItem> arrayList){
-        MultiItem item = new MultiItem();
-        item.setType(MultiItem.SLIDER_HORIZONTAL);
-        item.setArrayList(arrayList);
-        return item;
-    }
-
-    MultiItem SLIDER_VERTICAL(ArrayList<MultiItem> arrayList){
-        MultiItem item = new MultiItem();
-        item.setType(MultiItem.SLIDER_VERTICAL);
-        item.setArrayList(arrayList);
-        return item;
-    }
-
-    MultiItem ADD_STORY_ITEM(String title,String desc,String image,String soundName,String sound){
-        MultiItem item = new MultiItem();
-        item.setStory_title(title);
-        item.setStory_desc(desc);
-        item.setStory_image(image);
-        item.setStory_soundName(soundName);
-        item.setStory_sound(sound);
-        return item;
-    }
-
-
-    void additem_lakcheri(){
-        try {
-            JSONObject jsonObject = new JSONObject(getJsonMain());
-            Iterator iterator = jsonObject.keys();
-            while(iterator.hasNext()){
-                ArrayList<MultiItem> itemTwos = new ArrayList<>();
-                String key = (String)iterator.next();
-                JSONObject issue = jsonObject.getJSONObject(key);
-                //  get id from  issue
-                String name = issue.optString("name");
-                JSONArray array = issue.getJSONArray("array");
-
-                String sound_url = null;
-                for (int i = 0; i < array.length(); i++) {
-                    JSONObject object = array.getJSONObject(i);
-                    String title = object.getString("title");
-                    String desc = object.getString("desc");
-                    String image_url = object.getString("image_url");
-                    String sound_name = object.getString("sound_name");
-                    if (!object.isNull("sound_url"))
-                        sound_url = object.getString("sound_url");
-                    itemTwos.add(ADD_STORY_ITEM(title,desc,image_url,sound_name,sound_url));
-                }
-//                itemIntroList.add(SLIDER_L_H_V(itemTwos));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void additem_h(){
-        try {
-            JSONObject jsonObject = new JSONObject(getJsonMain());
-            Iterator iterator = jsonObject.keys();
-            while(iterator.hasNext()){
-                ArrayList<MultiItem> itemTwos = new ArrayList<>();
-                String key = (String)iterator.next();
-                JSONObject issue = jsonObject.getJSONObject(key);
-                //  get id from  issue
-                String name = issue.optString("name");
-                JSONArray array = issue.getJSONArray("array");
-
-                String sound_url = null;
-                for (int i = 0; i < array.length(); i++) {
-                    JSONObject object = array.getJSONObject(i);
-                    String title = object.getString("title");
-                    String desc = object.getString("desc");
-                    String image_url = object.getString("image_url");
-                    String sound_name = object.getString("sound_name");
-                    if (!object.isNull("sound_url"))
-                        sound_url = object.getString("sound_url");
-                    itemTwos.add(ADD_STORY_ITEM(title,desc,image_url,sound_name,sound_url));
-                }
-                itemIntroList.add(SLIDER_HORIZONTAL(itemTwos));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void additem_v(){
-        try {
-            JSONObject jsonObject = new JSONObject(getJsonMain());
-            Iterator iterator = jsonObject.keys();
-            while(iterator.hasNext()){
-                ArrayList<MultiItem> itemTwos = new ArrayList<>();
-                String key = (String)iterator.next();
-                JSONObject issue = jsonObject.getJSONObject(key);
-                //  get id from  issue
-                String name = issue.optString("name");
-                JSONArray array = issue.getJSONArray("array");
-
-                String sound_url = null;
-                for (int i = 0; i < array.length(); i++) {
-                    JSONObject object = array.getJSONObject(i);
-                    String title = object.getString("title");
-                    String desc = object.getString("desc");
-                    String image_url = object.getString("image_url");
-                    String sound_name = object.getString("sound_name");
-                    if (!object.isNull("sound_url"))
-                        sound_url = object.getString("sound_url");
-                    itemTwos.add(ADD_STORY_ITEM(title,desc,image_url,sound_name,sound_url));
-                }
-                itemIntroList.add(SLIDER_VERTICAL(itemTwos));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
     void JSON_SETER(){
@@ -371,5 +222,12 @@ public class MainActivity extends AppCompatActivity {
 
     String getJsonMain(){
         return SaveManager.get(this).getstring_appINFO().get(SaveManager.jsonMain);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }

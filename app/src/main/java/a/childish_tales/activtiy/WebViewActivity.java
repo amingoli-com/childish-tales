3723d8d8f852aafef295ae84c0f70cd58ad47b18
amingoli78 <amin.goli78@gmail.com>
2020-03-27 15:23:14 +0430

    add WebView

--- app/src/main/java/a/childish_tales/activtiy/WebViewActivity.java
+++ app/src/main/java/a/childish_tales/activtiy/WebViewActivity.java
@@ -1,16 +1,97 @@
 package a.childish_tales.activtiy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
 
 import a.childish_tales.R;
+import im.delight.android.webview.AdvancedWebView;
 
-public class WebViewActivity extends AppCompatActivity {
+public class WebViewActivity extends AppCompatActivity implements AdvancedWebView.Listener, SwipeRefreshLayout.OnRefreshListener {
+    String TAG = "amingoli78-WebViewActivity";
+    SwipeRefreshLayout swipeRefreshLayout;
+    AdvancedWebView mWebView;
+    String url = null;
 
+    @SuppressLint({"JavascriptInterface", "SetJavaScriptEnabled"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        url = getIntent().getStringExtra("url");
+
+        swipeRefreshLayout = findViewById(R.id.swiperefreshlayout);
+        mWebView = findViewById(R.id.webview);
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setRefreshing(true);
+
+        mWebView.setScrollbarFadingEnabled(false);
+        mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.setVerticalScrollBarEnabled(false);
+        mWebView.setScrollContainer(false);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setLoadsImagesAutomatically(true);
+        mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mWebView.getSettings().setSupportZoom(false);
+        mWebView.getSettings().setSavePassword(false);
+        mWebView.getSettings().setBlockNetworkImage(false);
+        mWebView.getSettings().setSupportMultipleWindows(false);
+        mWebView.getSettings().setAppCacheEnabled(true);
+        mWebView.addJavascriptInterface(this, "jsinterface");
+        mWebView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        mWebView.setListener(this, this);
+        mWebView.loadUrl(url);
+
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+        Log.d(TAG, "onPageStarted: ");
+        swipeRefreshLayout.setRefreshing(true);
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+        Log.d(TAG, "onPageFinished: ");
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+        Log.d(TAG, "onPageError: ");
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+
+    }
+
+    @Override
+    public void onExternalPageRequest(String url) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+        Log.d(TAG, "onRefresh: ");
+        mWebView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!mWebView.onBackPressed()) { return; }
+        super.onBackPressed();
     }
 }

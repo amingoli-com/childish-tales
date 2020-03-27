19e38f0e7fac85bbe63abc0467d7dccfb10c576b
amingoli78 <amin.goli78@gmail.com>
2020-03-27 16:06:43 +0430

    add activity video Player

---
+++ app/src/main/java/a/childish_tales/activtiy/VideoPlayerActivity.java
@@ -0,0 +1,16 @@
+package a.childish_tales.activtiy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import a.childish_tales.R;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}
